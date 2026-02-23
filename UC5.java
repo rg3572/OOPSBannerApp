156c55661dcbe50c65c0022246c9c9f7f698000a
Roshmi Ghosh <rg3572@srmist.edu.in>
2026-02-23 09:22:40 +0530

    Add files via upload

---
+++ UC5.java
@@ -0,0 +1,21 @@
+public class BannerDisplayApp {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            String.join("   ", "  *****  ", "  *****  ", " ******  ", "  *****  "),
+            String.join("   ", " **   ** ", " **   ** ", " **   ** ", " **   ** "),
+            String.join("   ", "**     **", "**     **", " **   ** ", " **      "),
+            String.join("   ", "**     **", "**     **", " ******  ", "  *****  "),
+            String.join("   ", "**     **", "**     **", " **      ", "       **"),
+            String.join("   ", "**     **", "**     **", " **      ", "       **"),
+            String.join("   ", "**     **", "**     **", " **      ", " **   ** "),
+            String.join("   ", " **   ** ", " **   ** ", " **      ", " **   ** "),
+            String.join("   ", "  *****  ", "  *****  ", " **      ", "  *****  ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
