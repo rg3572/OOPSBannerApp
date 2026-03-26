e1924ae41ba60aec0184526bfff8d0514e1a6bcd
Roshmi Ghosh <rg3572@srmist.edu.in>
2026-03-26 08:41:24 +0530

    Add files via upload

---
+++ HAUC4.java
@@ -0,0 +1,21 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+        String names;
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            names = nameBuilder.toString();
+        } else {
+            names = "World";
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
